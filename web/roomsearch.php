43e44725084b18405d75a6ef44e76973746fd4d8
Aleksey Popov <alexpopov366@hotmail.com>
2026-05-14 23:16:41 +0300

    Добавлена фиктивная функция each и вернул старый roomsearch*.php, работает

--- web/roomsearch.php
+++ web/roomsearch.php
@@ -15,16 +15,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
-require_once($CFG->libdir . '/outputcomponents.php');
-
-require_once "config.inc.php";
-require_once "functions.php";
-require_once "mrbs_auth.php";
-
-global $PAGE, $OUTPUT, $twentyfourhour_format;
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+include "config.inc.php";
+include "functions.php";
+require_once("mrbs_auth.php");
 
 require_login();
+global $twentyfourhour_format;
 
 $day = optional_param('day', 0, PARAM_INT);
 $month = optional_param('month', 0, PARAM_INT);
@@ -34,170 +31,136 @@ $edit_type = optional_param('edit_type', '', PARAM_ALPHA);
 $id = optional_param('id', 0, PARAM_INT);
 $room_id = optional_param('room_id', 0, PARAM_INT);
 $start_hour = optional_param('start_hour', 0, PARAM_INT);
+// $rep_type could use a closer look but I believe this is not passed via URL -ab.
 $start_min = optional_param('start_min', 0, PARAM_INT);
-$period = optional_param('period', 0, PARAM_INT);
 $rep_num_weeks = optional_param('rep_num_weeks', 0, PARAM_INT);
 $force = optional_param('force', false, PARAM_BOOL);
 $duration = optional_param('duration', 1, PARAM_INT);
 $all_day = optional_param('all_day', false, PARAM_BOOL);
-
 if ($enable_periods) {
     $default_dur_units = 'periods';
 } else {
     $default_dur_units = 'hours';
 }
+
 $dur_units = optional_param('dur_units', $default_dur_units, PARAM_TEXT);
 
-// If we dont know the right date then make it up.
-if (($day == 0) || ($month == 0) || ($year == 0)) {
-    $day = (int)date("d");
-    $month = (int)date("m");
-    $year = (int)date("Y");
+//If we dont know the right date then make it up
+if (($day == 0) or ($month == 0) or ($year == 0)) {
+    $day = date("d");
+    $month = date("m");
+    $year = date("Y");
 }
 
-$thisurl = new moodle_url('/blocks/mrbs/web/roomsearch.php', array(
-    'day' => $day,
-    'month' => $month,
-    'year' => $year,
-    'area' => $area
-));
-if (!empty($edit_type)) {
-    $thisurl->param('edit_type', $edit_type);
-}
-if (!empty($id)) {
-    $thisurl->param('id', $id);
-}
-if (!empty($room_id)) {
-    $thisurl->param('room_id', $room_id);
-}
-if (!empty($start_hour)) {
-    $thisurl->param('start_hour', $start_hour);
-}
-if (!empty($start_min)) {
-    $thisurl->param('start_min', $start_min);
-}
-if (!empty($period)) {
-    $thisurl->param('period', $period);
-}
-if (!empty($rep_num_weeks)) {
-    $thisurl->param('rep_num_weeks', $rep_num_weeks);
-}
-if (!empty($force)) {
-    $thisurl->param('force', $force);
-}
-if (!empty($duration)) {
-    $thisurl->param('duration', $duration);
-}
-if (!empty($all_day)) {
-    $thisurl->param('all_day', $all_day);
-}
-if (!empty($dur_units)) {
-    $thisurl->param('dur_units', $dur_units);
-}
-
-$PAGE->set_url($thisurl);
-$PAGE->set_context(context_system::instance());
-$PAGE->set_title(get_string('search'));
-$PAGE->set_heading(get_string('search'));
-$PAGE->requires->js(new moodle_url('/blocks/mrbs/web/roomsearch.js'));
-
-echo $OUTPUT->header();
 ?>
 
-<script type="text/javascript">
-    function openURL(sURL) {
-        opener.document.location = sURL;
-        window.close();
-    }
-
-    function OnAllDayClick() {
-        var allday = document.getElementById('all_day');
-        var form = document.forms["main"];
-        if (allday.checked) {
-            <?php if (!$enable_periods) { ?>
-            form.hour.value = "00";
-            form.minute.value = "00";
-            <?php } ?>
-            if (form.dur_units.value != "days") {
-                form.duration.value = "1";
-                form.dur_units.value = "days";
-            }
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <script type="text/JavaScript">
+        function openURL(sURL) {
+            opener.document.location = sURL;
+            window.close();
         }
-        RoomSearch();
-    }
-</script>
+    </script>
 
-<?php
-echo '<script type="text/javascript">SetWeekDayNames(';
-echo '"' . get_string('mon', 'calendar') . '", ';
-echo '"' . get_string('tue', 'calendar') . '", ';
-echo '"' . get_string('wed', 'calendar') . '", ';
-echo '"' . get_string('thu', 'calendar') . '", ';
-echo '"' . get_string('fri', 'calendar') . '", ';
-echo '"' . get_string('sat', 'calendar') . '", ';
-echo '"' . get_string('sun', 'calendar') . '"';
-echo ');</script>';
-?>
 
-<h2><?php echo get_string('search'); ?></h2>
+    <script type="text/javascript" src="roomsearch.js"></script>
+    <SCRIPT LANGUAGE="JavaScript">
+        function OnAllDayClick() // Executed when the user clicks on the all_day checkbox.
+        {
+            allday = document.getElementById('all_day');
+            form = document.forms["main"];
+            if (allday.checked) // If checking the box...
+            {
+                <?php if( !$enable_periods ) { ?>
+                form.hour.value = "00";
+                form.minute.value = "00";
+                <?php } ?>
+                if (form.dur_units.value != "days") // Don't change it if the user already did.
+                {
+                    form.duration.value = "1";
+                    form.dur_units.value = "days";
+                }
+            }
+            RoomSearch()
+        }
+    </SCRIPT>
+    <?php
+    // Set the weekday names for the 'ChangeOptionDays' function
+    echo '<script type="text/javascript">SetWeekDayNames(';
+    echo '"'.get_string('mon', 'calendar').'", ';
+    echo '"'.get_string('tue', 'calendar').'", ';
+    echo '"'.get_string('wed', 'calendar').'", ';
+    echo '"'.get_string('thu', 'calendar').'", ';
+    echo '"'.get_string('fri', 'calendar').'", ';
+    echo '"'.get_string('sat', 'calendar').'", ';
+    echo '"'.get_string('sun', 'calendar').'"';
+    echo ');</script>';
+    ?>
+</head>
+<body>
+<h2><?php echo get_string('search'); ?></H2>
 
 <div id="searchform">
-    <form name="main" action="#" method="get">
-        <table border="0">
-            <tr>
-                <td class="CR"><b><?php echo get_string('date') ?></b></td>
-                <td class="CL">
-                    <?php genDateSelector("", $day, $month, $year, false, true) ?>
-                    <script language="JavaScript">ChangeOptionDays(document.main, '');</script>
-                </td>
-            </tr>
+    <FORM NAME="main">
+        <TABLE BORDER=0>
 
+            <!-- Date selectors -->
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('date') ?></B></TD>
+                <TD CLASS=CL>
+                    <?php genDateSelector("", $day, $month, $year, false, true) ?>
+                    <SCRIPT LANGUAGE="JavaScript">ChangeOptionDays(document.main, '');</SCRIPT>
+                </TD>
+            </TR>
+
+            <!-- Start time/period selectors -->
             <?php if (!$enable_periods) { ?>
-                <tr>
-                    <td class="CR"><b><?php echo get_string('time') ?></b></td>
-                    <td class="CL">
-                        <input name="hour" size="2" value="<?php
-                            if (!$twentyfourhour_format && ($start_hour > 12)) {
-                                echo ($start_hour - 12);
-                            } else {
-                                echo $start_hour;
-                            }
-                        ?>" maxlength="2" onChange="RoomSearch()">:
-                        <input name="minute" size="2" value="<?php echo $start_min; ?>" maxlength="2" onChange="RoomSearch()">
+                <TR>
+                    <TD CLASS=CR><B><?php echo get_string('time') ?></B></TD>
+                    <TD CLASS=CL><INPUT NAME="hour" SIZE=2 VALUE="<?php if (!$twentyfourhour_format && ($start_hour > 12)) {
+                            echo($start_hour - 12);
+                        } else {
+                            echo $start_hour;
+                        } ?>" MAXLENGTH=2 onChange="RoomSearch()">:<INPUT NAME="minute" SIZE=2 VALUE="<?php echo $start_min; ?>"
+                                                                          MAXLENGTH=2 onChange="RoomSearch()">
                         <?php
                         if (!$twentyfourhour_format) {
                             $checked = ($start_hour < 12) ? "checked" : "";
-                            echo '<input name="ampm" type="radio" value="am" ' . $checked . '>' . userdate(mktime(1, 0, 0, 1, 1, 2000), "%p");
+                            echo "<INPUT NAME=\"ampm\" type=\"radio\" value=\"am\" $checked>".userdate(mktime(1, 0, 0, 1, 1, 2000), "%p");
                             $checked = ($start_hour >= 12) ? "checked" : "";
-                            echo '<input name="ampm" type="radio" value="pm" ' . $checked . '>' . userdate(mktime(13, 0, 0, 1, 1, 2000), "%p");
+                            echo "<INPUT NAME=\"ampm\" type=\"radio\" value=\"pm\" $checked>".userdate(mktime(13, 0, 0, 1, 1, 2000), "%p");
                         }
                         ?>
-                    </td>
-                </tr>
+                    </TD>
+                </TR>
             <?php } else { ?>
-                <tr>
-                    <td class="CR"><b><?php echo get_string('period', 'block_mrbs') ?></b></td>
-                    <td class="CL">
-                        <select name="period" onChange="RoomSearch()">
+                <TR>
+                    <TD CLASS=CR><B><?php echo get_string('period', 'block_mrbs') ?></B></TD>
+                    <TD CLASS=CL>
+                        <SELECT NAME="period" onChange="RoomSearch()">
                             <?php
                             foreach ($periods as $p_num => $p_val) {
-                                echo '<option value="' . $p_num . '"';
+                                echo "<OPTION VALUE=$p_num";
                                 if ((isset($period) && $period == $p_num) || $p_num == $start_min) {
-                                    echo ' selected';
+                                    echo " SELECTED";
                                 }
-                                echo '>' . s($p_val) . '</option>';
+                                echo ">$p_val";
                             }
                             ?>
-                        </select>
-                    </td>
-                </tr>
-            <?php } ?>
+                        </SELECT>
 
-            <tr>
-                <td class="CR"><b><?php echo get_string('duration', 'block_mrbs'); ?></b></td>
-                <td class="CL">
-                    <input name="duration" size="7" value="<?php echo (int)$duration; ?>" onChange="RoomSearch()" onKeyUp="RoomSearch()">
-                    <select name="dur_units" onChange="RoomSearch()">
+                    </TD>
+                </TR>
+
+                <!-- Duration selectors -->
+            <?php } ?>
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('duration', 'block_mrbs'); ?></B></TD>
+                <TD CLASS=CL><INPUT NAME="duration" SIZE=7 VALUE="<?php echo $duration; ?>" onChange="RoomSearch()"
+                                    onKeyUp="RoomSearch()">
+                    <SELECT NAME="dur_units" onChange="RoomSearch()">
                         <?php
                         if ($enable_periods) {
                             $units = array("periods", "days");
@@ -205,58 +168,64 @@ echo ');</script>';
                             $units = array("minutes", "hours", "days", "weeks");
                         }
 
-                        foreach ($units as $unit) {
-                            echo '<option value="' . s($unit) . '"';
-                            if ($dur_units == $unit || $dur_units == get_string($unit, 'block_mrbs')) {
-                                echo ' selected';
+                        while (list(, $unit) = each($units)) {
+                            echo "<OPTION VALUE=$unit";
+                            if ($dur_units == get_string($unit, 'block_mrbs')) {
+                                echo " SELECTED";
                             }
-                            echo '>' . get_string($unit, 'block_mrbs') . '</option>';
+                            echo " onChange=\"RoomSearch()\">".get_string($unit, 'block_mrbs');
                         }
                         ?>
-                    </select>
-                    <input name="all_day" type="checkbox" value="yes" id="all_day" <?php if ($all_day) { echo 'checked '; } ?>onClick="OnAllDayClick()">
-                    <?php echo get_string('all_day', 'block_mrbs'); ?>
-                </td>
-            </tr>
+                    </SELECT>
+                    <INPUT NAME="all_day" TYPE="checkbox" VALUE="yes" id="all_day" <?php if ($all_day) {
+                        echo 'CHECKED ';
+                    } ?>onClick="OnAllDayClick()"> <?php echo get_string('all_day', 'block_mrbs');
+                    if ($all_day) {
+                        echo '<body onload = "OnAllDayClick()"></body>';
+                    } ?>
+                </TD>
+            </TR>
 
-            <tr>
-                <td class="CR"><b><?php echo get_string('mincapacity', 'block_mrbs') ?></b></td>
-                <td class="CL"><input name="mincap" size="3" onChange="RoomSearch()" onKeyUp="RoomSearch()"></td>
-            </tr>
+            <!-- Capacity input-->
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('mincapacity', 'block_mrbs') ?></B></TD>
+                <TD CLASS=CL><INPUT NAME="mincap" SIZE="3" onChange="RoomSearch()" onKeyUp="RoomSearch()"></TD>
+            </TR>
 
-            <tr>
-                <td class="CR"><b><?php echo get_string('teachingroom', 'block_mrbs') ?></b></td>
-                <td class="CL"><input type="checkbox" name="teaching" onClick="RoomSearch()" checked="checked"></td>
-            </tr>
+            <!-- Teaching room input-->
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('teachingroom', 'block_mrbs') ?></B></TD>
+                <TD CLASS=CL><INPUT TYPE="checkbox" NAME="teaching" onClick="RoomSearch()" CHECKED></TD>
+            </TR>
 
-            <tr>
-                <td class="CR"><b><?php echo get_string('specialroom', 'block_mrbs') ?></b></td>
-                <td class="CL"><input type="checkbox" name="special" onClick="RoomSearch()" checked="checked"></td>
-            </tr>
+            <!-- Special room input-->
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('specialroom', 'block_mrbs') ?></B></TD>
+                <TD CLASS=CL><INPUT TYPE="checkbox" NAME="special" onClick="RoomSearch()" CHECKED></TD>
+            </TR>
 
-            <tr>
-                <td class="CR"><b><?php echo get_string('computerroom', 'block_mrbs') ?></b></td>
-                <td class="CL"><input type="checkbox" name="computer" onClick="RoomSearch()"></td>
-            </tr>
-        </table>
-    </form>
+            <!-- Computer room input-->
+            <TR>
+                <TD CLASS=CR><B><?php echo get_string('computerroom', 'block_mrbs') ?></B></TD>
+                <TD CLASS=CL><INPUT TYPE="checkbox" NAME="computer" onClick="RoomSearch()"></TD>
+            </TR>
+
+        </TABLE>
+    </FORM>
+
 </div>
 
+<!-- Area to display rooms found -->
 <h2 id="results"></h2>
-<?php
-echo '<table border="1"><thead><tr><th>' . get_string('area', 'block_mrbs') . '</th><th>Room</th><th>' .
-    get_string('description') . '</th><th>' . get_string('capacity', 'block_mrbs') .
-    '</th></tr></thead><tbody id="rooms"></tbody></table>';
-?>
+<?php echo '<table border=1 ><thead><tr><th>'.get_string('area', 'block_mrbs').'</th><th>Room</th><th>'.get_string('description').'</th><th>'.get_string('capacity', 'block_mrbs').'</th></tr></thead><tbody id="rooms"></tbody></table>'; ?>
 
-<script language="JavaScript">
+
+<SCRIPT LANGUAGE="JavaScript">
     var langRoomsFree = '<?php print_string('roomsfree', 'block_mrbs');?>';
     var langNoRooms = '<?php print_string('noroomsfound', 'block_mrbs');?>';
-    window.onload = function() {
-        RoomSearch();
-        <?php if ($all_day) { ?>OnAllDayClick();<?php } ?>
-    };
-</script>
+    window.onload = RoomSearch();
+</SCRIPT>
 
-<?php
-echo $OUTPUT->footer();
+
+</body>
+</html>
--- web/roomsearch_ss.php
+++ web/roomsearch_ss.php
@@ -15,23 +15,20 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
-
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
 global $DB;
-
-require_once "config.inc.php";
-require_once "functions.php";
-require_once "mrbs_auth.php";
-require_once "mrbs_sql.php";
+include "config.inc.php";
+include "functions.php";
+require_once('mrbs_auth.php');
+include "mrbs_sql.php";
 
 require_login();
-
 $day = optional_param('day', 0, PARAM_INT);
 $month = optional_param('month', 0, PARAM_INT);
 $year = optional_param('year', 0, PARAM_INT);
 $period = optional_param('period', 0, PARAM_INT);
-$durationraw = optional_param('duration', '0', PARAM_RAW_TRIMMED);
-$dur_units = optional_param('dur_units', '', PARAM_TEXT);
+$duration = optional_param('duration', 0, PARAM_INT);
+$dur_units = optional_param('dur_units', 0, PARAM_TEXT);
 $currentroom = optional_param('currentroom', 0, PARAM_INT);
 $mincap = optional_param('mincap', 0, PARAM_INT);
 $teaching = optional_param('teaching', false, PARAM_BOOL);
@@ -40,30 +37,29 @@ $computer = optional_param('computer', false, PARAM_BOOL);
 $hour = optional_param('hour', null, PARAM_INT);
 $minute = optional_param('minute', null, PARAM_INT);
 $ampm = optional_param('ampm', null, PARAM_ALPHA);
-$all_day = optional_param('all_day', false, PARAM_BOOL);
 
-$durationraw = preg_replace('/,/', '.', $durationraw);
-$duration = is_numeric($durationraw) ? (float)$durationraw : 0.0;
+global $tbl_room;
+global $tbl_entry;
+global $tbl_area;
 
-$resolution = isset($resolution) ? (int)$resolution : 3600;
+// Support locales where ',' is used as the decimal point
+$duration = preg_replace('/,/', '.', $duration);
 
 if ($enable_periods) {
     $resolution = 60;
     $hour = 12;
     $minute = $period;
     $max_periods = count($periods);
-
     if ($dur_units == "periods" && ($minute + $duration) > $max_periods) {
         $duration = (24 * 60 * floor($duration / $max_periods)) + ($duration % $max_periods);
     }
-
     if ($dur_units == "days" && $minute == 0) {
         $dur_units = "periods";
         $duration = $max_periods + ($duration - 1) * 60 * 24;
     }
 }
 
-// Units start in seconds.
+// Units start in seconds
 $units = 1.0;
 
 switch ($dur_units) {
@@ -82,31 +78,32 @@ switch ($dur_units) {
         break;
 }
 
-if ($all_day) {
+// Units are now in "$dur_units" numbers of seconds
+
+if (isset($all_day) && ($all_day == "yes")) {
     if ($enable_periods) {
         $starttime = mktime(12, 0, 0, $month, $day, $year);
         $endtime = mktime(12, $max_periods, 0, $month, $day, $year);
     } else {
         $starttime = mktime($morningstarts, 0, 0, $month, $day, $year);
         $end_minutes = $eveningends_minutes + $morningstarts_minutes;
-        if ($eveningends_minutes > 59) {
-            $end_minutes += 60;
-        }
+        ($eveningends_minutes > 59) ? $end_minutes += 60 : '';
         $endtime = mktime($eveningends, $end_minutes, 0, $month, $day, $year);
     }
 } else {
     if (!$twentyfourhour_format) {
-        if (!is_null($ampm) && ($ampm == "pm") && ($hour < 12)) {
+        if (isset($ampm) && ($ampm == "pm") && ($hour < 12)) {
             $hour += 12;
         }
-        if (!is_null($ampm) && ($ampm == "am") && ($hour > 11)) {
+        if (isset($ampm) && ($ampm == "am") && ($hour > 11)) {
             $hour -= 12;
         }
     }
 
-    $starttime = mktime((int)$hour, (int)$minute, 0, $month, $day, $year);
-    $endtime = $starttime + ($units * $duration);
-
+    $starttime = mktime($hour, $minute, 0, $month, $day, $year);
+    $endtime = mktime($hour, $minute, 0, $month, $day, $year) + ($units * $duration);
+    // Round up the duration to the next whole resolution unit.
+    // If they asked for 0 minutes, push that up to 1 resolution unit.
     $diff = $endtime - $starttime;
     if (($tmp = $diff % $resolution) != 0 || $diff == 0) {
         $endtime += $resolution - $tmp;
@@ -115,74 +112,41 @@ if ($all_day) {
     $endtime += cross_dst($starttime, $endtime);
 }
 
-$diff = $endtime - $starttime;
+$sql = "SELECT r.id, r.room_name, r.description, r.capacity, a.area_name, r.area_id";
+$sql .= " FROM {block_mrbs_room} r JOIN {block_mrbs_area} a on r.area_id=a.id WHERE ( SELECT COUNT(*) FROM {block_mrbs_entry} e ";
 
-$sql = "SELECT r.id, r.room_name, r.description, r.capacity, a.area_name, r.area_id
-          FROM {block_mrbs_room} r
-          JOIN {block_mrbs_area} a ON r.area_id = a.id
-         WHERE (
-                SELECT COUNT(*)
-                  FROM {block_mrbs_entry} e
-                 WHERE ((e.start_time >= ? AND e.end_time < ?)
-                     OR (e.start_time < ? AND e.end_time > ?)
-                     OR (e.start_time < ? AND e.end_time >= ?))
-                   AND e.room_id = r.id
-               ) < 1
-           AND r.capacity >= ?";
+//old booking fully inside new booking
+$sql .= "WHERE ((e.start_time>= ? AND e.end_time< ? ) ";
+//new start time within old booking
+$sql .= "OR (e.start_time< ? AND e.end_time> ?) ";
+//new end time within old booking
+$sql .= "OR (e.start_time< ? AND e.end_time>= ?)) ";
+
+$sql .= "AND e.room_id = r.id ) < 1  AND r.capacity >= ? ";
 
 $params = array($starttime, $endtime, $starttime, $starttime, $endtime, $endtime, $mincap);
 
-$descconditions = array();
-$descparams = array();
-
 if ($computer) {
-    $descconditions[] = $DB->sql_like('r.description', '?', false);
-    $descparams[] = 'Teaching IT%';
+    $sql .= " AND ".$DB->sql_like('r.description', '?', false);
+    $params[] = 'Teaching IT%';
 }
 if ($teaching) {
-    $descconditions[] = $DB->sql_like('r.description', '?', false);
-    $descparams[] = 'Teaching%';
+    $sql .= " AND ".$DB->sql_like('r.description', '?', false);
+    $params[] = 'Teaching%';
 }
 if ($special) {
-    $descconditions[] = $DB->sql_like('r.description', '?', false, false, true);
-    $descparams[] = 'Teaching Specialist%';
-}
-
-if (!empty($descconditions)) {
-    $sql .= ' AND (' . implode(' OR ', $descconditions) . ')';
-    $params = array_merge($params, $descparams);
+    $sql .= " AND ".$DB->sql_like('r.description', '?', false, false, true);
+    $params[] = 'Teaching Specialist%';
 }
 
 $sql .= " ORDER BY a.area_name, r.room_name";
 
 $rooms = $DB->get_records_sql($sql, $params);
-
 if (!empty($rooms)) {
     $list = '';
     foreach ($rooms as $room) {
-        $editurl = new moodle_url('/blocks/mrbs/web/edit_entry.php', array(
-            'room' => $room->id,
-            'period' => $period,
-            'year' => $year,
-            'month' => $month,
-            'day' => $day,
-            'duration' => $durationraw,
-            'dur_units' => $dur_units,
-            'all_day' => $all_day ? 1 : 0
-        ));
-        if (!$enable_periods) {
-            $editurl->param('hour', $hour);
-            $editurl->param('minute', $minute);
-            if (!is_null($ampm)) {
-                $editurl->param('ampm', $ampm);
-            }
-        }
-
-        $list .= s($room->area_name) . ','
-            . '<a href="javascript:openURL(\'' . $editurl->out(false) . '\')">' . s($room->room_name) . '</a>,'
-            . s($room->description) . ','
-            . (int)$room->capacity . "\n";
+        $list .= $room->area_name.',<a href="javascript:openURL(\'edit_entry.php?room='.$room->id.'&period='.$period.'&year='.$year.'&month='.$month.'&day='.$day.'&duration='.$diff.'\')">'.$room->room_name.'</a>,'.$room->description.','.$room->capacity."\n";
     }
-
+    //remove last \n to prevent blank row in table
     echo substr($list, 0, -1);
 }
