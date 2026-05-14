69b84a789b1e31d0c19332c7af222c3a4ca0ea0d
Aleksey Popov <alexpopov366@hotmail.com>
2026-05-14 23:14:06 +0300

    Добавлена фиктивная функция each, работает

--- web/functions.php
+++ web/functions.php
@@ -460,6 +460,25 @@ function round_t_up($t, $resolution, $am7) {
     }
 }
 
+if (!function_exists('each')) {
+    function each(array &$array) {
+        $key = key($array);
+
+        if ($key === null) {
+            return false;
+        }
+
+        $value = current($array);
+        next($array);
+
+        return array(
+            1 => $value,
+            'value' => $value,
+            0 => $key,
+            'key' => $key,
+        );
+    }
+}
 
 if (!function_exists('get_all_user_name_fields')) {
     /**
